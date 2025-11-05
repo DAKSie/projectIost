80fc550f4300b79dbcbf7b5ff20a0680ed93682a
DAKSie <redygrassy09@gmail.com>
2025-11-05 22:49:40 +0800

    feat: Created migrations script and completed services

--- Data/IostDbContext.cs
+++ Data/IostDbContext.cs
@@ -10,6 +10,12 @@ namespace projectIost.Data
         public DbSet<Item> Items { get; set; } = null;
 
         public DbSet<User> Users { get; set; } = null;
+
+        public DbSet<Order> Orders { get; set; } = null;
+        public DbSet<Order_Details> OrderDetails { get; set; } = null;
+
+        public DbSet<Supply> Supplies { get; set; } = null;
+        public DbSet<Supply_Details> SupplyDetails { get; set; } = null;
         // When adding other database tables use:
         // public DbSet<Order>, DbSet<Supplier>, etc
     }
